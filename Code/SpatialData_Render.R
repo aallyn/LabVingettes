78032847dda5c3e199c6ad20ed0771b6bb1d83de
Andrew Allyn <andrew.allyn@gmail.com>
2019-06-14 15:17:11 -0400

    Added time series components

--- Code/SpatialData_Render.R
+++ Code/SpatialData_Render.R
@@ -1,4 +1,4 @@
 library(here)
 library(rmarkdown)
 output.dir<- "./Temp Results/"
-render(here("Code", "SpatialData.Rmd"), output_dir = output.dir, params = list(output_dir = output.dir))
+render(here::here("Code", "SpatialData.Rmd"), output_dir = output.dir, params = list(output_dir = output.dir))
